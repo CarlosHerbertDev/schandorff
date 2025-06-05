98a3878a2f19c3a85779830d252e95e88303a859
CarlosHerbertDev <hrbrtvieira@gmail.com>
2025-06-05 16:22:02 -0300

    continuando estrutura inicial

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@api": path.resolve(__dirname, "src/api"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@contexts": path.resolve(__dirname, "src/contexts"),
+      "@mocks": path.resolve(__dirname, "src/mocks"),
+      "@interfaces": path.resolve(__dirname, "src/interfaces"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+    },
+  },
 })
